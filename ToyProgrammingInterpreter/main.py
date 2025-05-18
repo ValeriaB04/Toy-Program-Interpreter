4eec312d678f622ffeacf899ea06df50f6f9bb4c
DishaS2005 <dishasharmaa288@gmail.com>
2025-05-18 23:31:08 +0100

    Update mainv2.py

--- ToyProgrammingInterpreter/main.py
+++ ToyProgrammingInterpreter/main.py
@@ -1,12 +1,18 @@
-Class Interpreter: 
-  def __init__(self):
-    self.variables - {}
-    self.lines = []
-    self.current_line = 0
+
+class Interpreter: 
+    def __init__(self):
+        self.variables = {}
+        self.lines = []
+        self.current_line = 0
     
-def evaluate_expression():
-  try:
-    return eval(expr, {}, self.variable)
-  except Exception as e: 
-    print(f"Error evaluating expression '{expr}': {e}")
-    return 
+    def evaluate_expression(self,expr):
+      try:
+        return eval(expr, {}, self.variable)
+      except Exception as e: 
+        print(f"Error evaluating expression '{expr}': {e}")
+        return 
+    def assign_variable(self,key,value):
+        self.variables[key] = value
+        print(self.variables)
+interpreter = Interpreter()
+interpreter.assign_variable("hi",1)
