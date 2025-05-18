e6092ee19ce0d5baa70cc131ae222068d2e87b4b
ValeriaB04 <valeriabassan04@gmail.com>
2025-05-18 23:46:19 +0100

    Update main.py

--- ToyProgrammingInterpreter/main.py
+++ ToyProgrammingInterpreter/main.py
@@ -4,15 +4,31 @@ class Interpreter:
         self.variables = {}
         self.lines = []
         self.current_line = 0
-    
-    def evaluate_expression(self,expr):
+
+    # Convert expression string into a value 
+    def eval_expr(self,expr):
       try:
         return eval(expr, {}, self.variable)
       except Exception as e: 
         print(f"Error evaluating expression '{expr}': {e}")
         return 
+
+    # Assign a variable in the environment 
     def assign_variable(self,key,value):
         self.variables[key] = value
         print(self.variables)
+
+    # let print_stmt: str -> None
+    def print_stmt(self, expr) -> None:
+      value = self.eval_expr(expr)
+      print(value) 
+
+    # Execute a while loop given a condition and a start line to loop back to
+    def while_loop(self, condition, start_line, current_line):
+        if self.eval_expr(condition):
+            return start_line  
+        else:
+            return current_line + 1
+        
 interpreter = Interpreter()
 interpreter.assign_variable("hi",1)
